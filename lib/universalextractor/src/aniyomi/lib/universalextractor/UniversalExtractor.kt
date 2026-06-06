d78bf58fe8ac64c1959face2d3e3353a91a479a8
adly98 <111687237+adly98@users.noreply.github.com>
2026-06-07 01:50:52 +0400

    Update(lib/universalextractor, tuktukcinema)

--- lib/universalextractor/src/aniyomi/lib/universalextractor/UniversalExtractor.kt
+++ lib/universalextractor/src/aniyomi/lib/universalextractor/UniversalExtractor.kt
@@ -1,119 +1,77 @@
 package aniyomi.lib.universalextractor
 
-import android.annotation.SuppressLint
-import android.app.Application
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import android.webkit.WebResourceRequest
-import android.webkit.WebResourceResponse
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import aniyomi.lib.jsunpacker.JsUnpacker
 import aniyomi.lib.playlistutils.PlaylistUtils
+import aniyomi.lib.synchrony.Deobfuscator
 import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
-import uy.kohesive.injekt.injectLazy
-import java.util.Locale
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 
-class UniversalExtractor(private val client: OkHttpClient) {
-    private val context: Application by injectLazy()
-    private val handler by lazy { Handler(Looper.getMainLooper()) }
+class UniversalExtractor(private val client: OkHttpClient, private val headers: Headers) {
 
-    @SuppressLint("SetJavaScriptEnabled")
-    fun videosFromUrl(origRequestUrl: String, origRequestHeader: Headers, customQuality: String? = null, prefix: String = ""): List<Video> {
-        val host = origRequestUrl.toHttpUrl().host.substringBefore(".").proper()
-        val latch = CountDownLatch(1)
-        var webView: WebView? = null
-        var resultUrl = ""
-        val playlistUtils by lazy { PlaylistUtils(client, origRequestHeader) }
-        val headers = origRequestHeader.toMultimap().mapValues { it.value.getOrNull(0) ?: "" }.toMutableMap()
+    private val playlistUtils = PlaylistUtils(client)
 
-        handler.post {
-            val newView = WebView(context)
-            webView = newView
-            with(newView.settings) {
-                javaScriptEnabled = true
-                domStorageEnabled = true
-                databaseEnabled = true
-                useWideViewPort = false
-                loadWithOverviewMode = false
-                userAgentString = origRequestHeader["User-Agent"]
+    fun videosFromUrl(url: String, host: String = "", resolution: String? = ""): List<Video> {
+        val prefix = host.ifBlank {
+            url.toHttpUrl().host.substringBefore('.')
+        }.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
+        val quality = resolution?.ifBlank { "Mirror" }
+        val document = client.newCall(GET(url, headers)).execute().asJsoup()
+        val videoHeaders = headers.newBuilder().set("Referer", url).build()
+
+        val videoSources = document.select("source")
+        if (videoSources.isNotEmpty()) {
+            return videoSources.map {
+                val src = it.attr("src")
+                Video(src, "$prefix: $quality", src)
             }
-            newView.webViewClient = object : WebViewClient() {
-                override fun shouldInterceptRequest(
-                    view: WebView,
-                    request: WebResourceRequest,
-                ): WebResourceResponse? {
-                    val url = request.url.toString()
-                    if (VIDEO_REGEX.containsMatchIn(url)) {
-                        resultUrl = url
-                        latch.countDown()
-                    }
-                    return super.shouldInterceptRequest(view, request)
+        }
+
+        val videoMatches = document.select("script").flatMap { scriptElement ->
+            scriptElement.data().takeIf { it.isNotEmpty() }?.let {
+                VIDEO_URL_REGEX.findAll(resolveScript(it)).toList()
+            } ?: emptyList()
+        }
+
+        if (videoMatches.isNotEmpty()) {
+            return videoMatches.flatMap { match ->
+                val videoUrl = match.value
+                if (videoUrl.contains("mp4")) {
+                    Video(videoUrl, "$prefix: $quality", videoUrl, headers = videoHeaders).let(::listOf)
+                } else {
+                    playlistUtils.extractFromHls(videoUrl, url, videoNameGen = { streamQuality -> "$prefix: $streamQuality" })
                 }
             }
-
-            webView?.loadUrl(origRequestUrl, headers)
         }
-
-        latch.await(TIMEOUT_SEC, TimeUnit.SECONDS)
-
-        handler.post {
-            webView?.stopLoading()
-            webView?.destroy()
-            webView = null
-        }
-        // terabox special case start
-        if ("M3U8_AUTO_360" in resultUrl) {
-            val qualities = listOf("1080", "720", "480", "360")
-            val allVideos = mutableListOf<Video>()
-
-            for (quality in qualities) {
-                val modifiedUrl = resultUrl.replace("M3U8_AUTO_360", "M3U8_AUTO_$quality")
-                val videos = playlistUtils.extractFromHls(modifiedUrl, origRequestUrl, videoNameGen = { "$prefix - $host: $it ${quality}p" })
-
-                if (videos.isNotEmpty()) {
-                    allVideos.addAll(videos)
-                }
-            }
-
-            if (allVideos.isNotEmpty()) {
-                return allVideos
-            }
-        }
-        // terabox special case end
-
-        return when {
-            "m3u8" in resultUrl -> {
-                Log.d("UniversalExtractor", "m3u8 URL: $resultUrl")
-                playlistUtils.extractFromHls(resultUrl, origRequestUrl, videoNameGen = { "$prefix - $host: $it" })
-            }
-            "mpd" in resultUrl -> {
-                Log.d("UniversalExtractor", "mpd URL: $resultUrl")
-                playlistUtils.extractFromDash(resultUrl, { it -> "$prefix - $host: $it" }, referer = origRequestUrl)
-            }
-            "mp4" in resultUrl -> {
-                Log.d("UniversalExtractor", "mp4 URL: $resultUrl")
-                Video(resultUrl, "$prefix - $host: ${customQuality ?: "Mirror"}", resultUrl, origRequestHeader.newBuilder().add("referer", origRequestUrl).build()).let(::listOf)
-            }
-            else -> emptyList()
-        }
+        return emptyList()
     }
 
-    private fun String.proper(): String = this.replaceFirstChar {
-        if (it.isLowerCase()) {
-            it.titlecase(Locale.getDefault())
+    /**
+     * Resolves and deobfuscates a potentially obfuscated or packed JavaScript string.
+     *
+     * @param script The JavaScript code as a string to be resolved.
+     * @return The deobfuscated and unpacked JavaScript code as a string.
+     */
+    private fun resolveScript(script: String): String {
+        val deobfuscatedScript = if (OBFUSCATED_SCRIPT_REGEX.containsMatchIn(script)) {
+            Deobfuscator.deobfuscateScript(script) ?: script
         } else {
-            it.toString()
+            script
         }
+        val unpackedScript = if (PLAYER_SCRIPT_REGEX.containsMatchIn(deobfuscatedScript)) {
+            JsUnpacker.unpackAndCombine(deobfuscatedScript) ?: deobfuscatedScript
+        } else {
+            deobfuscatedScript
+        }
+        return unpackedScript
     }
 
     companion object {
-        const val TIMEOUT_SEC: Long = 10
-        private val VIDEO_REGEX by lazy { Regex(".*\\.(mp4|m3u8|mpd)(\\?.*)?$") }
+        private val PLAYER_SCRIPT_REGEX by lazy { Regex("""(?i)eval\s*\(\s*.*?(player|file|source|mp4|m3u8).*?\)""") }
+        private val VIDEO_URL_REGEX by lazy { Regex("""https?://[^\s"'<>\\]{7,}\.(?:m3u8|mpd|mp4)(?:\?[^"'<>\\]*)?""") }
+        private val OBFUSCATED_SCRIPT_REGEX by lazy { Regex("""(\\\\/|\\x[0-9a-f]{2}|\\u[0-9a-f]{4}|\\[0-7]{1,3}|\\b[a-zA-Z0-9+/]{20,}={0,2}\\b|\[[^]]*]\[[^]]*]|${'$'}_[A-Za-z0-9]+|atob|String\.fromCharCode|decodeURIComponent)""", RegexOption.IGNORE_CASE) }
     }
 }
